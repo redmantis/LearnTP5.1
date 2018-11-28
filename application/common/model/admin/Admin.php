012533155ef076858865e54a671a76b4c73eb2f2
RDM <RDM@RDM-PC>
2018-11-28 23:25:26 +0800

    第四天  添加一条数据

--- application/common/model/admin/Admin.php
+++ application/common/model/admin/Admin.php
@@ -5,7 +5,56 @@ use think\Model;
 use think\Db;
 
 class Admin extends Model {
-    public function getlist(){
+
+//    protected $table = "zf_rooms";
+
+//    public function initialize() {
+//        $prefix = config('database.prefix');
+//         $this->table = $prefix . $this->table; //当前基表全名
+//         $this->table = $prefix . "zf_rooms"; //当前基表全名
+//        parent::initialize();
+//    }
+    
+    /**
+     * 用户名获取器
+     * @param type $value
+     * @return type
+     */    
+    public function setUserNameAttr($value)
+    {
+        return strtolower($value);
+    }
+    
+    public function setPwdAttr($value) {
+        $salt = "123456";
+        $this->salt = $salt;
+        return md5(md5($value) . $salt);     
+    }
+
+    public function setSaltAttr($value) {
+        return $value;
+    }
+
+    /**
+     * 用户名修改器
+     * @param type $value
+     * @return type
+     */
+    public function getUserNameAttr($value){
+        return strtoupper($value);
+    }
+    
+    
+    public function getAddtimeAttr($value) {
+        return date("Y-m-d H:i:s", $value);
+    }
+
+    /**
+     * 数据列表获取
+     * @return type
+     */
+    public function getlist() {
         return $this->select();
-    } 
+    }
+
 }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,17 +6,38 @@ use think\Controller;
 class Index extends Controller {
 
     public function index() {
+       
+//        添加一条数据
+//        第一种是实例化模型对象后赋值并保存：
+        $admin=new \app\common\model\admin\Admin();
+        $admin->user_name = "新增用户";
+        $admin->pwd = "1234567";
+        $admin->addtime = time();
+        $admin->status = 1;
+        $admin->save();
         
-//        var_dump(config('database.prefix'));
-//        return;
+//        直接传入数据到save方法批量赋值
+        $admin->save(['user_name'=>'直接传入数据','pwd'=>'pwd','addtime'=> time()]);
         
-        $db=new \app\common\model\admin\Admin();
-        $rs=$db->getlist();        
-        foreach ($rs as $admin) {     
-            echo "{$admin['id']}-{$admin['name']}-{$admin['truename']}<br />";
+//        或者直接在实例化的时候传入数据
+         $db=new \app\common\model\admin\Admin(['user_name'=>'直接实例化传值','pwd'=>'pwd','addtime'=> time()]);
+         $db->save();
+         
+        /**
+         * 数据查询
+         */
+        $rs=$admin->getlist();        
+        foreach ($rs as $v) {     
+            echo "{$v['id']}-{$v['user_name']}-{$v['addtime']}<br />";
         }
-//        $rs = db('admin')->where('status',1)->select();
+
 //        var_dump($rs);
+//          $data['id']=21;
+//        $data['title'] = "whesssre";
+//        $db->data($data, true);
+//        $db->save();
+//        $r = $db->update($data);
+//        var_dump($r);
         return;
 //        return $this->fetch();
     }
