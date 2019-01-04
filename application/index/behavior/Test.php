1763078611579244a43e02eec67b75c8fd9048fb
RDM <RDM@RDM-PC>
2019-01-04 08:45:58 +0800

    调整模板位置

--- application/index/behavior/Test.php
+++ application/index/behavior/Test.php
@@ -7,7 +7,7 @@ class Test {
 
     public function run($params) {
         $view_base = config('template.view_base');
-        $new_view_base = str_replace("__THEME_NAME__", "views", $view_base); //替换主题名称
-        View :: config('view_base', $new_view_base); //完成配置修改   
+        $new_view_base = str_replace(["__MODULAR_NAME__","__THEME_NAME__"], ["pc","theme1"], $view_base); //替换主题名称
+        View :: config('view_base', $new_view_base); //完成配置修改           
     }
 }
