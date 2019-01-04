4b8cf2b41e163367829d15ac510325313ada4414
RDM <RDM@RDM-PC>
2019-01-04 19:52:34 +0800

    第四天 简单写入数据及皮肤目录结构的组织

--- application/index/behavior/Test.php
+++ application/index/behavior/Test.php
@@ -6,9 +6,9 @@ use think\facade\View;
 class Test {
 
     public function run($params) {
-        $view_base = config('template.view_base');
-        $new_view_base = str_replace(["__MODULAR_NAME__", "__THEME_NAME__"], ["pc", "theme1"], $view_base); //替换主题名称
-        View :: config('view_base', $new_view_base); //完成配置修改       
+//        $view_base = config('template.view_base');
+//        $new_view_base = str_replace(["__MODULAR_NAME__", "__THEME_NAME__"], ["pc", "theme1"], $view_base); //替换主题名称
+//        View :: config('view_base', $new_view_base); //完成配置修改       
 
         $view_path = config('template.view_path');
         $new_view_path = str_replace(["__MODULAR_NAME__", "__THEME_NAME__"], ["pc", "theme1"], $view_path); //替换主题名称
